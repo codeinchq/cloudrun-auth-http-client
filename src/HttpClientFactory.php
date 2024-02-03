da09968ec38a15813eabf798f25ffd64ac67160b
Joan Fabrégat <joan@codeinc.co>
2024-02-02 16:01:16 -0800

    v1

---
+++ src/HttpClientFactory.php
@@ -0,0 +1,74 @@
+<?php
+/*
+ * Copyright (c) 2024 Code Inc. - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Visit <https://www.codeinc.co> for more information
+ */
+
+declare(strict_types=1);
+
+namespace CodeInc\CloudrunAuthHttpClient;
+
+use Google\Auth\Credentials\ServiceAccountCredentials;
+use Google\Auth\Middleware\AuthTokenMiddleware;
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+
+/**
+ * @see https://github.com/googleapis/google-auth-library-php
+ */
+final readonly class HttpClientFactory
+{
+    /**
+     * @param array|string $jsonKey
+     * @param string $serviceUrl
+     * @return Client
+     */
+    public function factory(array|string $jsonKey, string $serviceUrl): Client
+    {
+        return new Client([
+            'handler'  => $this->createHttpClientHandler($jsonKey, $serviceUrl),
+            'auth'     => 'google_auth',
+            'base_uri' => $serviceUrl,
+        ]);
+    }
+
+    /**
+     * @param array|string $jsonKey
+     * @param string $serviceUrl
+     * @return HandlerStack
+     */
+    private function createHttpClientHandler(array|string $jsonKey, string $serviceUrl): HandlerStack
+    {
+        $stack = HandlerStack::create();
+        $stack->push($this->createMiddleware($jsonKey, $serviceUrl));
+        return $stack;
+    }
+
+    /**
+     * @param array|string $jsonKey
+     * @param string $serviceUrl
+     * @return AuthTokenMiddleware
+     */
+    private function createMiddleware(array|string $jsonKey, string $serviceUrl): AuthTokenMiddleware
+    {
+        return new AuthTokenMiddleware(
+            $this->getServiceAccountCredentials($jsonKey, $serviceUrl)
+        );
+    }
+
+    /**
+     * @param array|string $jsonKey
+     * @param string $targetAudience
+     * @return ServiceAccountCredentials
+     */
+    private function getServiceAccountCredentials(array|string $jsonKey, string $targetAudience): ServiceAccountCredentials
+    {
+        return new ServiceAccountCredentials(
+            scope: null,
+            jsonKey: $jsonKey,
+            targetAudience: $targetAudience
+        );
+    }
+}
